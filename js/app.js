bfddcbeaa5013e3a00d12ed9c6180c5ac9924ab3
phookajoe <smith_richard4@bah.com>
2014-08-13 15:28:42 -0600

    First upload

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+/*$(window).load(function(){
+  $('#masonryContainer').masonry({  
+    itemSelector: '.masonry-brick',
+    columnWidth: 240
+  });  
+});   */ 
